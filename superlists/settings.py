0d8dd8297e9c71126303f70fd9d3b37911440c5c
myrmica-habilis <michalportes1@gmail.com>
2020-05-16 17:26:15 +0200

    Fix bug with ALLOWED_HOSTS

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if 'DJANGO_DEBUG_FALSE' in os.environ:
     DEBUG = False
     SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
-    ALLOWED_HOSTS = os.environ['SITENAME']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
 else:
     DEBUG = True
     SECRET_KEY = 'n#_n5pvuieb#^vc8pkwrk7q=q2rn50_dje58ah!^6r=30(3j!$'
